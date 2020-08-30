59aea7675bc27604eb324ebee4e00b15652229e7
Barry Yu <doung1118@gmail.com>
2020-08-30 21:55:15 +0800

    在app.js路由上新增app.use>>指定到靜態路由的區域public,及新增一個導覽列

--- app.js
+++ app.js
@@ -16,11 +16,24 @@ app.set('view engine', 'handlebars')
 // routes setting
 app.get('/', (req, res) => {
 
-  // res.send('')
-  //將這邊的ＨＴＭＬ內榮移置到index.handlebars 
+  // res.send('原本的ＨＴＭＬ內容移轉到~~~/views/index.handlebars')
+
+
   res.render('index')
 
+  // Render 是表示view 對bowser 渲染畫面 .  檔案來自於 index.handlebars
+
 })
+
+
+
+
+//-------------------------------  告訴 路由器 指向到 public 靜態檔案區域的位置 ------------------------
+
+
+app.use(express.static('public'))
+
+
 
 // start and listen on the Express server
 app.listen(port, () => {
